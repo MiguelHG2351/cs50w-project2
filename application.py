d87ed17f3f97ceb8d6d2fb8a846fde7f1b89c36f
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-04-26 15:58:35 -0600

    idea para enviar archivos

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from flask_socketio import SocketIO, emit
+# from io import BytesIO
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -11,8 +12,14 @@ socketio = SocketIO(app)
 
 # Puedo emitir un evento del cliente hacia el servidor
 # Puedo enviar un evento al client como emit igualmente
+# para enviar los recursos puedo usar rutas dinamicas como /image/<algo>
 
 @app.route("/")
 def index():
 
     return render_template("index.html")
+
+@app.route("/imagen/<name>")
+def image(name):
+
+    return send_file(f'./static/images/{name}')
